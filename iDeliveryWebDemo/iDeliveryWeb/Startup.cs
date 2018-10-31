9e0e95f6eec6b8e7c6960c576410c1c314d6477c
Gabriel Ichim <ichimgabriel75@gmail.com>
2018-10-31 11:39:16 +0000

    Initial Comit
    Start Project

---
+++ iDeliveryWebDemo/iDeliveryWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(iDeliveryWeb.Startup))]
+namespace iDeliveryWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

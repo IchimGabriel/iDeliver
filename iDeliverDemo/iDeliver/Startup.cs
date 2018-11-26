85bdc4f1a9bf9f37d5c8ea09d21aaf4a8fcf096a
Grigore Bivol <36542313+bivolgri@users.noreply.github.com>
2018-11-26 16:01:12 +0000

    comment out CreateRolesAndUsers

--- iDeliverDemo/iDeliver/Startup.cs
+++ iDeliverDemo/iDeliver/Startup.cs
@@ -13,75 +13,76 @@ namespace iDeliver
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
-            CreateRolesAndUsers();
+            //CreateRolesAndUsers();
         }
 
-        private void CreateRolesAndUsers()
-        {
-            ApplicationDbContext context = new ApplicationDbContext();
+        //private void CreateRolesAndUsers()
+        //{
+        //    ApplicationDbContext context = new ApplicationDbContext();
 
-            var RoleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
-            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+        //    var RoleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+        //    var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
 
-            // In Startup iam creating first Admin Role and creating a default Admin User 
-            if (!RoleManager.RoleExists("Admin"))
-            {
+        //    // In Startup iam creating first Admin Role and creating a default Admin User 
+        //    // test changes Grig
+        //    if (!RoleManager.RoleExists("Admin"))
+        //    {
 
-                // first we create Admin rool
-                var role = new IdentityRole
-                {
-                    Name = "Admin"
-                };
-                RoleManager.Create(role);
+        //        // first we create Admin rool
+        //        var role = new IdentityRole
+        //        {
+        //            Name = "Admin"
+        //        };
+        //        RoleManager.Create(role);
 
-                //Here we create a Admin super user who will maintain the website				
+        //        //Here we create a Admin super user who will maintain the website				
 
-                var user = new ApplicationUser
-                {
-                    UserName = "Grigore",
-                    Email = "bivolgri@yahoo.com"
-                };
+        //        var user = new ApplicationUser
+        //        {
+        //            UserName = "Grigore",
+        //            Email = "bivolgri@yahoo.com"
+        //        };
 
-                string userPass = "g*******1";
+        //        string userPass = "g*******1";
 
-                var checkUser = UserManager.Create(user, userPass);
+        //        var checkUser = UserManager.Create(user, userPass);
 
-                //Add default User to Role Admin
-                if (checkUser.Succeeded)
-                {
-                    var result1 = UserManager.AddToRole(user.Id, "Admin");
-                }
-            }
+        //        //Add default User to Role Admin
+        //        if (checkUser.Succeeded)
+        //        {
+        //            var result1 = UserManager.AddToRole(user.Id, "Admin");
+        //        }
+        //    }
 
-            // create Shop Manager role 
-            if (!RoleManager.RoleExists("ShopMng"))
-            {
-                var role = new IdentityRole
-                {
-                    Name = "ShopMng"
-                };
-                RoleManager.Create(role);
-            }
+        //    // create Shop Manager role 
+        //    if (!RoleManager.RoleExists("ShopMng"))
+        //    {
+        //        var role = new IdentityRole
+        //        {
+        //            Name = "ShopMng"
+        //        };
+        //        RoleManager.Create(role);
+        //    }
 
-            // create Driver role 
-            if (!RoleManager.RoleExists("Driver"))
-            {
-                var role = new IdentityRole
-                {
-                    Name = "Driver"
-                };
-                RoleManager.Create(role);
-            }
+        //    // create Driver role 
+        //    if (!RoleManager.RoleExists("Driver"))
+        //    {
+        //        var role = new IdentityRole
+        //        {
+        //            Name = "Driver"
+        //        };
+        //        RoleManager.Create(role);
+        //    }
 
-            // create Driver role 
-            if (!RoleManager.RoleExists("Analyst"))
-            {
-                var role = new IdentityRole
-                {
-                    Name = "Analyst"
-                };
-                RoleManager.Create(role);
-            }
-        }
+        //    // create Driver role 
+        //    if (!RoleManager.RoleExists("Analyst"))
+        //    {
+        //        var role = new IdentityRole
+        //        {
+        //            Name = "Analyst"
+        //        };
+        //        RoleManager.Create(role);
+        //    }
+        //}
     }
 }

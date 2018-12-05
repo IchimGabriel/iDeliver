6e8b747f43eebaebc1e3cf766a6c2045687f0644
Gabriel Ichim <ichimgabriel75@gmail.com>
2018-12-05 19:59:04 +0000

    Work on Analyst

---
+++ iDeliverDemo/iDeliver/Controllers/AnalystController.cs
@@ -0,0 +1,51 @@
+﻿using iDeliver.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Data;
+using System.Xml;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace iDeliver.Controllers
+{
+    public class AnalystController : Controller
+    {
+        private readonly ApplicationDbContext db = new ApplicationDbContext();
+
+
+        // GET: Analyst
+        [HttpGet]
+        public ActionResult Index()
+        {
+
+            var data = db.Orders.Where(s => s.ShopIdentity != null).OrderByDescending(s => s.Total).ToList(); //.Include(s => s.Driver).Include(s => s.Shop);
+            var ototal = data.Where(s => s.Total != 0);
+            List<int> id = new List<int>();
+            List<decimal> total = new List<decimal>();
+
+            foreach (var item in ototal)
+            {
+                if (item.Total > 0)
+                {
+                    id.Add(item.OrderId);
+                    total.Add(item.Total);
+                }
+            }
+            var myChart = new Chart(width: 800, height: 600, theme: ChartTheme.Green)
+                .AddTitle("Orders")
+                .AddSeries("All Orders",
+                    xValue: id, xField: "Id",
+                    yValues: total, yFields: "Total")
+                .AddLegend()
+                .Write();
+
+            ///ViewBag.Chart = myChart;
+
+            return View();
+        }
+    }
+}
---
+++ iDeliverDemo/iDeliver/Startup.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using iDeliver.Models;
+using Owin;
+
+
+namespace iDeliver
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            //CreateRolesAndUsers();
+        }
+        #region CREATE ADMIN USER
+
+        //private void CreateRolesAndUsers()
+        //{
+        //    ApplicationDbContext context = new ApplicationDbContext();
+
+        //    var RoleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+        //    var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+        //    // In Startup iam creating first Admin Role and creating a default Admin User 
+         
+        //    if (!RoleManager.RoleExists("Administrator"))
+        //    {
+
+        //        // first we create Admin rool
+        //        var role = new IdentityRole
+        //        {
+        //            Name = "Administrator"
+        //        };
+        //        RoleManager.Create(role);
+
+        //        //Here we create a Admin super user who will maintain the website				
+
+        //        var user = new ApplicationUser
+        //        {
+        //            UserName = "Gabriel",
+        //            Email = "ichimgabriel@gmail.com"
+        //        };
+
+        //        string userPass = "";
+
+        //        var checkUser = UserManager.Create(user, userPass);
+
+        //        //Add default User to Role Admin
+        //        if (checkUser.Succeeded)
+        //        {
+        //            var result1 = UserManager.AddToRole(user.Id, "Administrator");
+        //        }
+        //    }
+
+        //    // create Shop Manager role 
+        //    if (!RoleManager.RoleExists("ShopManager"))
+        //    {
+        //        var role = new IdentityRole
+        //        {
+        //            Name = "ShopManager"
+        //        };
+        //        RoleManager.Create(role);
+        //    }
+
+        //    // create Driver role 
+        //    if (!RoleManager.RoleExists("Driver"))
+        //    {
+        //        var role = new IdentityRole
+        //        {
+        //            Name = "Driver"
+        //        };
+        //        RoleManager.Create(role);
+        //    }
+
+        //    // create Driver role 
+        //    if (!RoleManager.RoleExists("Analyst"))
+        //    {
+        //        var role = new IdentityRole
+        //        {
+        //            Name = "Analyst"
+        //        };
+        //        RoleManager.Create(role);
+        //    }
+        //}
+        #endregion
+    }
+}

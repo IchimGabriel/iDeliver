750147a3d4b2dd6ca547c89b5ba3c069d38aff61
Grigore Bivol <36542313+bivolgri@users.noreply.github.com>
2018-12-08 18:19:20 +0000

    Updated Tests and Re-created the FakeTestData Class (designated for testing)

---
+++ iDeliverDemo/iDeliver.Test/FakeTestData.cs
@@ -0,0 +1,37 @@
+﻿using iDeliver.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iDeliver.Test
+{
+    public class FakeTestData
+    {
+       
+
+       public  static List<Driver> fakeDrivers = new List<Driver>()
+        {
+           new Driver() { DriverId=1, DriverIdentity="George", Name="George", OnDelivery=false, OnLine=false},
+           new Driver() { DriverId=2, DriverIdentity="John", Name="John", OnDelivery=false, OnLine=false},
+        };
+       public  static List<Shop> fakeShops = new List<Shop>()
+        {
+           new Shop() { Name="Pizza", Open=true,  ShopId=1, ShopIdentity="Pizza" },
+           new Shop() { Name="Kebab", Open=false,  ShopId=2, ShopIdentity="Kebab" },
+        };
+        public static List<Order> fakeOrders = new List<Order>()
+        {
+            new Order() { OrderId=1, Address="23 Main Street", Total=55, Commission = 4, DriverIdentity="George", IsDelivered=false, TimeStamp= DateTime.Now, ShopIdentity="Pizza" },
+            new Order() { OrderId=2, Address="23 The Grove", Total=63, Commission = 5, DriverIdentity="Bob", IsDelivered=true, TimeStamp= DateTime.Now, ShopIdentity="Pizza" }
+        };
+
+        
+
+
+    }
+       
+}
--- iDeliverDemo/iDeliver.Test/iDeliverTest.cs
+++ iDeliverDemo/iDeliver.Test/iDeliverTest.cs
@@ -2,9 +2,12 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+
 
 
 namespace iDeliver.Test
@@ -12,25 +15,23 @@ namespace iDeliver.Test
     [TestClass()]
     public class iDeliverTest
     {
-        
+        private decimal totalTest = 0;
 
         [TestMethod()]
         public void DriverTest()
-        {              
+        {
             //arrange and act
             Driver dr = new Driver() { DriverId = 1000, DriverIdentity = "GG", Name = "George Green", OnDelivery = true, OnLine = true };
-
-            //assert
             Assert.IsInstanceOfType(dr, typeof(Driver));
-            Assert.AreEqual(dr.DriverId, 1000);
+            Assert.AreEqual(dr.DriverId,1000); 
             Assert.AreEqual(dr.Name, "George Green");
-            Assert.AreEqual(dr.OnDelivery,true);
+            Assert.AreEqual(dr.OnDelivery, true);
         }
 
         [TestMethod()]
         public void OrderTest()
         {
-            Order or = new Order() {Address = "Test Street", IsDelivered = true };
+            Order or = new Order() { Address = "Test Street", IsDelivered = true };
 
             //assert
             Assert.IsInstanceOfType(or, typeof(Order));
@@ -42,7 +43,7 @@ namespace iDeliver.Test
         [TestMethod()]
         public void ShopTest()
         {
-            var sh = new Shop() { Name="Royal Pizza", ShopId=23 };
+            var sh = new Shop() { Name = "Royal Pizza", ShopId = 23 };
 
             //assert
             Assert.IsInstanceOfType(sh, typeof(Shop));
@@ -51,10 +52,43 @@ namespace iDeliver.Test
 
         }
 
+       
+        [TestMethod]
+        public void GetSumTest()
+        {
+            foreach (var item in FakeTestData.fakeOrders)
+            {
+                totalTest += item.Total;
+            }
+
+            Assert.AreEqual(totalTest, GetTotalSum(FakeTestData.fakeOrders));
+
+        }
+
+        public decimal GetTotalSum(List<Order> orders)
+        {
+            decimal totalsum = 0;
+
+            foreach (var item in orders)
+            {
+                totalsum += item.Total;
+
+            }
+
+            return totalsum;
+
+        }
+
+        
 
 
     }
 
-
-
 }
+
+
+
+
+
+
+    

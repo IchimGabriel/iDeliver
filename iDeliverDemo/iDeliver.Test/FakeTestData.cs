f01d2a0d215060a238ae2ab0edaece1d00e068c8
Grigore Bivol <36542313+bivolgri@users.noreply.github.com>
2018-12-08 16:11:00 +0000

    Testing Classes Driver Order Shop

--- iDeliverDemo/iDeliver.Test/FakeTestData.cs
+++ iDeliverDemo/iDeliver.Test/FakeTestData.cs
@@ -5,44 +5,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using iDeliver.Models;
+
 
 namespace iDeliver.Test
 {
     [TestClass()]
     public class iDeliverTest
     {
+        
+
         [TestMethod()]
         public void DriverTest()
-        {
+        {              
+            //arrange and act
             Driver dr = new Driver() { DriverId = 1000, DriverIdentity = "GG", Name = "George Green", OnDelivery = true, OnLine = true };
+
+            //assert
             Assert.IsInstanceOfType(dr, typeof(Driver));
             Assert.AreEqual(dr.DriverId, 1000);
             Assert.AreEqual(dr.Name, "George Green");
             Assert.AreEqual(dr.OnDelivery,true);
-            
-            
-            //List<Driver> testDriver = new List<Driver>()
-            // {
-            // new Driver() { DriverId=1, DriverIdentity="George", Name="George", OnDelivery=false, OnLine=false},
-            // Assert.AreEqual(testDriver.DriverID)
-
-
-            // };
-             //new Driver() { DriverId=2, DriverIdentity="John", Name="John", OnDelivery=false, OnLine=false},
-             
         }
-        static List<Shop> fakeShops = new List<Shop>()
-       {
-           new Shop() { Name="Pizza", Open=true,  ShopId=1, ShopIdentity="Pizza" },
-           new Shop() { Name="Kebab", Open=false,  ShopId=2, ShopIdentity="Kebab" },
-       };
 
-        static List<Order> fakeOrders = new List<Order>()
+        [TestMethod()]
+        public void OrderTest()
         {
-            new Order() { OrderId=1, Address="23 Main Street", Total=55, Commission = 4, DriverIdentity="George", IsDelivered=false, TimeStamp= DateTime.Now, ShopIdentity="Pizza" }
+            Order or = new Order() {Address = "Test Street", IsDelivered = true };
 
-        };
+            //assert
+            Assert.IsInstanceOfType(or, typeof(Order));
+            Assert.AreEqual(or.Address, "Test Street");
+            Assert.AreNotEqual(or.IsDelivered, false);
+
+        }
+
+        [TestMethod()]
+        public void ShopTest()
+        {
+            var sh = new Shop() { Name="Royal Pizza", ShopId=23 };
+
+            //assert
+            Assert.IsInstanceOfType(sh, typeof(Shop));
+            Assert.AreEqual(sh.Name, "Royal Pizza");
+            Assert.AreNotEqual(sh.ShopId, 12);
+
+        }
+
+
 
     }
 
